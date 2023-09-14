3f47814ff1e0e83edece34ee2d0c0705c7b887d9
Anguh Bleaise <bleaise.anguh.quigo@gmail.com>
2023-09-14 19:25:52 +0100

    writing query strings

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
- import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.tsx'
 import '@/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ import { Provider } from "react-redux";
+ import { configureStore } from "@reduxjs/toolkit";
+ import { setupListeners } from "@reduxjs/toolkit/query";
+ import { api } from "@/state/api";
+
+ export const store = configureStore({
+     reducer: { [api.reducerPath]: api.reducer },
+     middleware: (getDefault) => getDefault().concat(api.middleware),
+ });
+ setupListeners(store.dispatch);
+
+ ReactDOM.createRoot(document.getElementById('root')!).render(
+     <Provider store={store}>
+         <App />
+     </Provider>
 )
